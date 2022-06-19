28c13480adbf9cc35407071bbff19f6755ef68f3
AlenaVladymtsova <alenakhikhlukha@tut.by>
2022-06-19 23:21:18 +0300

    Homework SQL DDL ver 2

--- dz2_SQL_DDL_gr28_Vladymtsova_Alena.sql
+++ dz2_SQL_DDL_gr28_Vladymtsova_Alena.sql
@@ -1,6 +1,4 @@
-
-
-/* 1)	Создать таблицу employees
+п»ї/* 1)	РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employees
 - id. serial,  primary key,
 - employee_name. Varchar(50), not null */
 create table employees(
@@ -8,7 +6,7 @@ id serial primary key,
 employee_name varchar(50) not null
 )
 
-/*2)	Наполнить таблицу employee 70 строками. */
+/*2)	РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee 70 СЃС‚СЂРѕРєР°РјРё. */
 insert into employees (employee_name)
 values ('Anna'),
        ('Alla'),
@@ -95,7 +93,7 @@ values ('Anna'),
        ('Max_2'),
        ('Alex_2');
       
-/*3)	Создать таблицу salary
+/*3)	РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ salary
 - id. Serial  primary key,
 - monthly_salary. Int, not null
  */      
@@ -103,7 +101,9 @@ create table salary (
      id serial primary key,
      montly_salary int not null
      )
-/* 4)	Наполнить таблицу salary 15 строками*/       
+
+
+/* 4)	РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ salary 15 СЃС‚СЂРѕРєР°РјРё*/       
 insert into salary(montly_salary)
 values (1000),
        (1100),
@@ -119,10 +119,9 @@ values (1000),
        (2100),
        (2200),
        (2300),
-       (2400),
-       (2500);
+       (2400);
        
-     /* 5)	Создать таблицу employee_salary
+     /* 5)	РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary
 - id. Serial  primary key,
 - employee_id. Int, not null, unique
 - salary_id. Int, not null
@@ -130,14 +129,12 @@ values (1000),
     create table employee_salary (
     id serial primary key,
     employee_id int not null unique,
-    salary_id int not null,
-    foreign key(employee_id)
-    references employees(id),
-    foreign key(salary_id)
-    references salary(id))
-       
-    /* 6)	Наполнить таблицу employee_salary 40 строками:
-- в 10 строк из 40 вставить несуществующие employee_id
+    salary_id int not null
+  )
+      
+ select * from roles_employee;  
+    /* 6)	РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary 40 СЃС‚СЂРѕРєР°РјРё:
+- РІ 10 СЃС‚СЂРѕРє РёР· 40 РІСЃС‚Р°РІРёС‚СЊ РЅРµСЃСѓС‰РµСЃС‚РІСѓСЋС‰РёРµ employee_id
 */
 insert into employee_salary (employee_id, salary_id)
 values (1,18),
@@ -169,9 +166,19 @@ values (1,18),
        (27,14),
        (28,19),
        (29,14),
-       (30,20);
+       (30,20),
+       (71,20),
+       (72,17),
+       (73,15),
+       (74,12),
+       (75,14),
+       (76,21),
+       (77,14),
+       (78,19),
+       (79,14),
+       (90,20);       
               
-      /*7)	Создать таблицу roles
+      /*7)	РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles
 - id. Serial  primary key,
 - role_name. int, not null, unique
  */
@@ -179,11 +186,11 @@ values (1,18),
       id serial primary key,
       role_name int not null unique
       )
-/*8)	Поменять тип столба role_name с int на varchar(30) */    
+/*8)	РџРѕРјРµРЅСЏС‚СЊ С‚РёРї СЃС‚РѕР»Р±Р° role_name СЃ int РЅР° varchar(30) */    
 alter table roles 
 alter column role_name type varchar(30);    
 
-/*9)	Наполнить таблицу roles 20 строками */
+/*9)	РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles 20 СЃС‚СЂРѕРєР°РјРё */
 insert into roles (role_name)
 values ('Junior Python developer'),
        ('Middle Python developer'),
@@ -206,10 +213,10 @@ values ('Junior Python developer'),
        ('Middle Automation QA engineer'),
        ('Senior Automation QA engineer');
        
-/* 10)	Создать таблицу roles_employee
+/* 10)	РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee
 - id. Serial  primary key,
-- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
-- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+- employee_id. Int, not null, unique (РІРЅРµС€РЅРёР№ РєР»СЋС‡ РґР»СЏ С‚Р°Р±Р»РёС†С‹ employees, РїРѕР»Рµ id)
+- role_id. Int, not null (РІРЅРµС€РЅРёР№ РєР»СЋС‡ РґР»СЏ С‚Р°Р±Р»РёС†С‹ roles, РїРѕР»Рµ id)
 */
 create table roles_employee (
     id serial primary key,
@@ -220,7 +227,7 @@ create table roles_employee (
     foreign key(role_id)
     references roles(id)) 
     
-   /* 11)	Наполнить таблицу roles_employee 40 строками*/
+   /* 11)	РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee 40 СЃС‚СЂРѕРєР°РјРё*/
 insert into roles_employee (employee_id, role_id)
 values (1,2),
        (2,4),
